b23335549ffc7b53cdbe2e1cfb47a5fbb9fa7bd6
LOST0LOSER <jim807260650@gmail.com>
2020-12-06 16:02:40 +0800

    fix login

--- client/src/classes/OrderInfo.js
+++ client/src/classes/OrderInfo.js
@@ -1,35 +1,13 @@
-class OrderInfo {
-    uid = undefined;
-    name = undefined;
-    info = undefined;
-    currentSummoningCount = undefined;
-    maximumSummoningCount = undefined;
-    status = undefined;
-    date = undefined;
-    master_id = undefined;
-    type = undefined;
-    photos = [];
+import Order from './Order.js';
+export default class OrderInfo extends Order {
     constructor(args) {
-        if (args instanceof Object) {
-            this.uid = args['uid']
-            this.name = args['name'];
-            this.info = args['info'];
-            this.currentSummoningCount = args['currentSummoningCount'];
-            this.maximumSummoningCount = args['maximumSummoningCount'];
-            this.status = args['status'];
-            this.date = args['date'];
-            this.master_id = args['master_id'];
-            this.type = args['type'];
-            this.photos = args['photos'];
-        }
+        super(args);
+
     }
-    setPhotos(array){
-        this.photos = array;
-    }
-    addPhotos(array){
-        for(let each of array){
+
+    addPhotos(array) {
+        for (let each of array) {
             this.photos.push(each);
         }
     }
 }
-export default OrderInfo;
